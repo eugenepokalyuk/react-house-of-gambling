f1972dfae1610a400e99180618ee002bd936c7c5
Evgenii <eugene.pokalyuk@gmail.com>
2023-10-13 12:53:32 +0700

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './components/App/App';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from "./services/store";
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <Provider store={store}>
+    {/* <BrowserRouter> */}
+    <HashRouter>
+      <App />
+    </HashRouter>
+    {/* </BrowserRouter> */}
+  </Provider>
+);
